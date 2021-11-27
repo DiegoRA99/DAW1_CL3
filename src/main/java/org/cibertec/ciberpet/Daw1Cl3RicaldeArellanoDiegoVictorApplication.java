2ef0eeaeeaded764f6329e9205c565ae7e65562b
DiegoRA99 <Diego_ricalde@icloud.com>
2021-11-26 20:23:28 -0500

    fiest commit

---
+++ src/main/java/org/cibertec/ciberpet/Daw1Cl3RicaldeArellanoDiegoVictorApplication.java
@@ -0,0 +1,13 @@
+package org.cibertec.ciberpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Daw1Cl3RicaldeArellanoDiegoVictorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Daw1Cl3RicaldeArellanoDiegoVictorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/cibertec/ciberpet/Daw1Cl3RicaldeArellanoDiegoVictorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cibertec.ciberpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Daw1Cl3RicaldeArellanoDiegoVictorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
